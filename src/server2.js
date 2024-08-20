a50328edb93d8ecc30905d406e201f102d076ba8
Joseph Ajayi <ajayimiles73@gmail.com>
2024-08-20 17:09:42 +0100

    created a second server to distribute the workload

---
+++ src/server2.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { config } from "./config/env.js";
+import { userRouter } from './users/users.routes.js';
+import { reviewsRouter } from './reviews/reviews.routes.js';
+import { tokenRouter } from './tokens/tokens.routes.js';
+import { paymentRouter } from './payments/paymnents.routes.js';
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.use("/user", userRouter);
+app.use("/reviews", reviewsRouter);
+app.use ("/token", tokenRouter);
+app.use("/payment", paymentRouter);
+
+
+app.listen( config.port2, async() => {
+    console.log("server's running on port", config.port2);
+});
